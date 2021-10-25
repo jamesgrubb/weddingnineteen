1a742892033543e4780660e7010b0b184ca611ff
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2021-10-25 08:45:39 +0100

    Fixed food allergies

--- components/Forms/UpdateGuest/index.js
+++ components/Forms/UpdateGuest/index.js
@@ -1,4 +1,9 @@
-import { useForm, useWatch } from 'react-hook-form';
+import {
+	useForm,
+	useWatch,
+	FormProvider,
+	useFormContext,
+} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { object, string } from 'yup';
 import Item from '../../../components/Menu/Item';
@@ -9,6 +14,7 @@ const validationSchema = object().shape({
 });
 
 const Menu = ({ control, intolerances }) => {
+	const methods = useFormContext();
 	const items = useWatch({
 		control,
 	});
@@ -41,7 +47,7 @@ const Menu = ({ control, intolerances }) => {
 							<label htmlFor={item.id}>{item.fields.Name}</label>
 							<input
 								type='checkbox'
-								// {...register(`Intolerance`)}
+								{...methods.register(`Diet`)}
 								id={item.id}
 								value={item.fields.Name}
 							/>
@@ -55,15 +61,15 @@ const Menu = ({ control, intolerances }) => {
 
 export const UpdateGuest = ({ formItems, guest, intolerances, ...props }) => {
 	const { Name } = guest[0].fields;
-
-	const { register, control, formState, handleSubmit } = useForm({
+	const methods = useForm({
 		resolver: yupResolver(validationSchema),
 		defaultValues: {
 			Starter: '',
 			Main: '',
-			Intolerance: [],
+			Diet: [],
 		},
 	});
+	const { register, control, formState, handleSubmit } = methods;
 
 	const { errors } = formState;
 
@@ -85,92 +91,96 @@ export const UpdateGuest = ({ formItems, guest, intolerances, ...props }) => {
 	});
 
 	return (
-		<form className='menu-form' onSubmit={handleSubmit(onSubmit, onError)}>
-			<div className='menu-form__view'>
-				<div className='menu-form__view-content'>
-					<Menu intolerances={intolerances} control={control} />
-					<div className={styles.menuSubmit}>
-						<p>
-							Happy with your choice? Send your order. otherwise,
-							choose gain. If you change your mind after clicking
-							the send button. Start over
-						</p>
-						<button type='submit' className='btn'>
-							<span>send order</span>
-						</button>
+		<FormProvider {...methods}>
+			<form
+				className='menu-form'
+				onSubmit={handleSubmit(onSubmit, onError)}>
+				<div className='menu-form__view'>
+					<div className='menu-form__view-content'>
+						<Menu intolerances={intolerances} control={control} />
+						<div className={styles.menuSubmit}>
+							<p>
+								Happy with your choice? Send your order.
+								otherwise, choose gain. If you change your mind
+								after clicking the send button. Start over
+							</p>
+							<button type='submit' className='btn'>
+								<span>send order</span>
+							</button>
+						</div>
+						{/* <input className='btn' type='submit' /> */}
 					</div>
-					{/* <input className='btn' type='submit' /> */}
 				</div>
-			</div>
-			<div className='menu-form__choice'>
-				<div className='menu-form__choice-header'>
-					<h4>Starters</h4>
-				</div>
-				{starters.map((starter) => {
-					return (
-						<div
-							className='menu-form__form-group serif'
-							key={starter.id}>
-							<div className='menu-form__form-label'>
-								<label htmlFor={starter.id}>
-									{starter.fields.Dish}
-								</label>
+				<div className='menu-form__choice'>
+					<div className='menu-form__choice-header'>
+						<h4>Starters</h4>
+					</div>
+					{starters.map((starter) => {
+						return (
+							<div
+								className='menu-form__form-group serif'
+								key={starter.id}>
+								<div className='menu-form__form-label'>
+									<label htmlFor={starter.id}>
+										{starter.fields.Dish}
+									</label>
+								</div>
+								<div className='menu-form__form-input'>
+									<input
+										type='radio'
+										{...register(`Starter`)}
+										value={starter.fields.Nickname}
+										id={starter.id}
+									/>
+								</div>
 							</div>
-							<div className='menu-form__form-input'>
-								<input
-									type='radio'
-									{...register(`Starter`)}
-									value={starter.fields.Nickname}
-									id={starter.id}
-								/>
-							</div>
-						</div>
-					);
-				})}
-				{errors?.Starter ? (
-					<div className='error'>{errors?.Starter?.message}</div>
-				) : (
-					''
-				)}
+						);
+					})}
+					{errors?.Starter ? (
+						<div className='error'>{errors?.Starter?.message}</div>
+					) : (
+						''
+					)}
 
-				<div className='menu-form__choice-header'>
-					<h4>Mains</h4>
-				</div>
-				{mains.map((main) => {
-					return (
-						<div
-							className='menu-form__form-group serif'
-							key={main.id}>
-							<div className='menu-form__form-label'>
-								<label htmlFor={main.id}>
-									{main.fields.Dish}
-								</label>
+					<div className='menu-form__choice-header'>
+						<h4>Mains</h4>
+					</div>
+					{mains.map((main) => {
+						return (
+							<div
+								className='menu-form__form-group serif'
+								key={main.id}>
+								<div className='menu-form__form-label'>
+									<label htmlFor={main.id}>
+										{main.fields.Dish}
+									</label>
+								</div>
+								<div className='menu-form__form-input'>
+									<input
+										key={main.id}
+										type='radio'
+										id={main.id}
+										{...register(`Main`)}
+										value={main.fields.Nickname}
+									/>
+								</div>
 							</div>
-							<div className='menu-form__form-input'>
-								<input
-									key={main.id}
-									type='radio'
-									id={main.id}
-									{...register(`Main`)}
-									value={main.fields.Nickname}
-								/>
-							</div>
-						</div>
-					);
-				})}
-				{errors?.Main ? (
-					<label className='error'>{errors?.Main?.message}</label>
-				) : (
-					''
-				)}
+						);
+					})}
+					{errors?.Main ? (
+						<label className='error'>{errors?.Main?.message}</label>
+					) : (
+						''
+					)}
 
-				<div className='menu-form__choice-header'>
-					<h4>Desert</h4>
+					<div className='menu-form__choice-header'>
+						<h4>Desert</h4>
+					</div>
+					{deserts.map((item) => {
+						return <Item key={item.id} name={item.fields.Dish} />;
+					})}
 				</div>
-				{deserts.map((item) => {
-					return <Item key={item.id} name={item.fields.Dish} />;
-				})}
-			</div>
-		</form>
+			</form>
+		</FormProvider>
 	);
 };
